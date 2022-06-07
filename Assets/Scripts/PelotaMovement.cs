3442a7d239768712a0808445c99ded2b1a61cbc1
LucaFedericoMarty <101400526+LucaFedericoMarty@users.noreply.github.com>
2022-06-07 20:00:20 -0300

    Movimiento Actualizacion

--- Assets/Scripts/PelotaMovement.cs
+++ Assets/Scripts/PelotaMovement.cs
@@ -11,16 +11,20 @@ public class PelotaMovement : MonoBehaviour
 
     // Variables Int
     int cantidadDeTiros;
+    int contadorDeTiros;
 
     // Variable Float
     float timeElapsed;
-    float horizonatlSpeed = 2.0f;
-    float verticalSpeed = -0.1f;
+    public float horizonatlSpeed;
+    public float verticalSpeed;
     public  float rotation;
     public float translation;
+
+
     Rigidbody rb;
     public Text contadorTiempo;
-    int contadorDeTiros;
+
+    public GameObject CameraPelota;
 
     // Start is called before the first frame update
     void Start()
@@ -33,44 +37,60 @@ public class PelotaMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        timeElapsed += Time.deltaTime;
         contadorTiempo.text = Mathf.Floor(timeElapsed).ToString();
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            transform.eulerAngles += new Vector3(horizonatlSpeed, 0, 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            transform.eulerAngles -= new Vector3(horizonatlSpeed, 0, 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Mouse0) || Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            rb.AddForce(verticalSpeed, 0, 0);
+        }
 
         // Si el click izquierdo es presionado, poner a isPressing como true
 
-        if (Input.GetKeyDown(KeyCode.Mouse0))
-        {           
-            isPressing = true;
-        }
+        //if (Input.GetKeyDown(KeyCode.Mouse0))
+        //{           
+        //    isPressing = true;
+        //}
 
-        // Si se esta presionando el click izquierdo:
-        // Contar el tiempo por cuanto lo hizo
-        // Trackear cual fue el movimiento del mouse en el Eje X, luego multiplicar este valor por la velocidad de rotacion
-        // Trackear cual fue el movimiento del mouse en el Eje Y, luego multiplicar este valor por la velocidad de traslacion
+        //// Si se esta presionando el click izquierdo:
+        //// Contar el tiempo por cuanto lo hizo
+        //// Trackear cual fue el movimiento del mouse en el Eje X, luego multiplicar este valor por la velocidad de rotacion
+        //// Trackear cual fue el movimiento del mouse en el Eje Y, luego multiplicar este valor por la velocidad de traslacion
 
-        if (isPressing == true)
-        {
-            timeElapsed += Time.deltaTime;
-            rotation = horizonatlSpeed * Input.GetAxis("Mouse X");
-            translation = verticalSpeed * Input.GetAxis("Mouse Y");
-        }
+        //if (isPressing == true)
+        //{
+        //    timeElapsed += Time.deltaTime;
+        //    rotation = horizonatlSpeed * Input.GetAxis("Mouse X");
+        //    translation = verticalSpeed * Input.GetAxis("Mouse Y");
+        //}
 
-        // Si dejo de presionar el click izquierdo:
-        // La variable que monitorea el estado del click se torna falsa
-        // Se multiplica tanto el valor de rotacion como de traslacion por Time.DeltaTime, para que la velocidad no dependa de los FPS
-        // Se ejecuta el tiro
+        //// Si dejo de presionar el click izquierdo:
+        //// La variable que monitorea el estado del click se torna falsa
+        //// Se multiplica tanto el valor de rotacion como de traslacion por Time.DeltaTime, para que la velocidad no dependa de los FPS
+        //// Se ejecuta el tiro
 
-        if (Input.GetKeyUp(KeyCode.Mouse0))
-        {
-            rotation *= Time.deltaTime;
-            translation *= Time.deltaTime;
-            //transform.eulerAngles += new Vector3(0, 0, rotation);
-            //transform.Translate(translation, 0, 0);
-            rb.AddForce(transform.eulerAngles += new Vector3(0,0, rotation) * translation,ForceMode.Impulse);
-            //rb.AddForce(0, 0, translation);
-            isPressing = false;
-            timeElapsed = 0;
-            contadorDeTiros++;
-        }
+        //if (Input.GetKeyUp(KeyCode.Mouse0))
+        //{
+        //    rotation *= Time.deltaTime;
+        //    translation *= Time.deltaTime;
+        //    //transform.eulerAngles += new Vector3(0, 0, rotation);
+        //    //transform.Translate(translation, 0, 0);
+        //    rb.AddForce(transform.eulerAngles += new Vector3(0,0, rotation) * translation,ForceMode.Impulse);
+        //    //rb.AddForce(0, 0, translation);
+        //    isPressing = false;
+        //    timeElapsed = 0;
+        //    contadorDeTiros++;
+        //}
 
 
         if (transform.position.y < 0 && contadorDeTiros <= cantidadDeTiros )
